3ba90fb77574713453ff1f10666c2d2630d5bb08
William Hall <billhall168@gmail.com>
2021-03-15 04:43:14 -0700

    hamburger menu update

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,18 @@
-const btnHamburger = document.querySelector('#btnHamburger')
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function () {
     console.log('open hamburger')
 
-    if (btnHamburger.classList.contains('open')) {
-        btnHamburger.classList.remove('open')
-    } else {
-        btnHamburger.classList.add('open')
+    if (header.classList.contains('open')) { // close burger menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else { // open burger menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 })
 // ****************GLOBAL VARIABLES **************************
